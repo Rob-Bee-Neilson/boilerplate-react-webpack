4937ec43af13cc3e0cfcd8781f7fdc17126ae39a
Rob-Bee-Neilson <robbie.neilson@gmail.com>
2019-02-04 14:49:10 +1300

    extracted data successfully into blogs.js

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,63 +5,24 @@ import Blogbox from './Blogbox'
 import blogs from '../../server/public/blogs'
 
 
-const testblog = [
-  {
-    title: 'Title one',
-    date: '12/34/56',
-    content: 'aah ahh aah',
-    image: '/images/download.jpg'
-  },
-  {
-    title: 'Title two point five',
-    date: '78/90/12',
-    content: 'blah blah blah',
-    image: '/images/download2.jpg'
-  },
-  {
-    title: 'Title 3',
-    date: '12/34/56',
-    content: 'aah ahh aah',
-    image: '/images/download.jpg'
-  },
-  {
-    title: 'Title 4',
-    date: '12/34/56',
-    content: 'aah ahh aah',
-    image: '/images/download.jpg'
-  },
-  {
-    title: 'Title 5',
-    date: '12/34/56',
-    content: 'aah ahh aah',
-    image: '/images/download.jpg'
-  }
-]
 
 
 
 const App = () => {
   
-  // showBlog(blog) {
-  //   return (
-  //     <Blogbox key={blog.title}
-  //       title={blog.title}
-  //       date={blog.date}
-  //       content={blog.content}
-  //       image={blog.image}
-  //     />
-  //   )
-  // }
+  
 
   return (
 
     <div className='container'>
       <h1>Rob's Blog v1.0</h1>
 
-      {console.log({blogs})}
+      {console.log({
+        blogs})}
       
-      {testblog.map(blog => {
+      {blogs.map(blog => {
         return  <Blogbox key={blog.title}
+        keyword={blog.keyword}
         title={blog.title}
         date={blog.date}
         content={blog.content}
--- server/public/blogs.js
+++ server/public/blogs.js
@@ -4,7 +4,7 @@ module.exports = [
         keyword: 'robbie',
         title: 'Title one',
         date: '12/34/56',
-        entry: 'aah ahh aah',
+        content: 'aah ahh aah',
         image: '/images/download.jpg'
       },
       {
@@ -12,7 +12,7 @@ module.exports = [
         keyword: 'baby',
         title: 'Title two',
         date: '23/45/67',
-        entry: 'b b b b b ',
+        content: 'b b b b b ',
         image: '/images/download2.jpg'
       },
       {
@@ -20,7 +20,7 @@ module.exports = [
         keyword: 'robbie',
         title: 'Title three',
         date: '45/67/89',
-        entry: 'c c c c c c',
+        content: 'c c c c c c',
         image: '/images/download.jpg'
       }
 ]
