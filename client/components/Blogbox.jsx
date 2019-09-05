99678563c8407a6dd70ca0c05c3c32deff7dbc98
rob-bee-neilson <robbie.neilson@gmail.com>
2019-09-05 13:03:43 +1200

    test card in blogbox

--- client/components/Blogbox.jsx
+++ client/components/Blogbox.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import Header from './Header'
 import Content from './Content'
 import Image from './Image'
 
 const Blogbox = props => {
-
+  
   return(
 
     <div className='blog-wrapper'>
@@ -21,6 +25,32 @@ const Blogbox = props => {
       <div className='blog-image'>
         <Image image={props.image} alt={props.alt} />
       </div><br></br>
+
+      <Card className={'x1'}>
+      <CardContent>
+        <Typography className={'x2'} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        {/* <Typography variant="h5" component="h2">
+          be
+          {bull}
+          nev
+          {bull}o{bull}
+          lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography> */}
+      </CardContent>
+      {/* <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions> */}
+    </Card>
 
     </div>
 
