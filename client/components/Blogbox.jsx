f7df5771ddddf6869fa2341277bbab31187ea174
rob-bee-neilson <robbie.neilson@gmail.com>
2019-09-09 22:50:44 +1200

    update blogbox with Mui

--- client/components/Blogbox.jsx
+++ client/components/Blogbox.jsx
@@ -4,10 +4,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { sizing } from '@material-ui/system';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import Header from './Header'
 import Content from './Content'
-import Image from './Image'
+// import Image from './Image'
 import { typography } from '@material-ui/system';
 
 const Blogbox = props => {
@@ -17,41 +20,41 @@ const Blogbox = props => {
       maxWidth: 345,
     },
     media: {
-      height: 140,
+      
+      maxWidth: 345,
     },
   });
 
   const classes = useStyles();
 
+  
   return(
 
     <div className='blog-wrapper'>
 
-      {/* <div className='blog-header'>
-        <Header title={props.title} date={props.date} />
-      </div><br></br>
-
-      <div className='blog-content'>
-        <Content content={props.content} />
-      </div><br></br>
-
-      <div className='blog-image'>
-      <Image image={props.image} alt={props.alt} />
-      </div><br></br> */}
-
       <Card className={classes.card}>
-      
-        <CardContent>
-          <Typography className={'x2'} color="textSecondary" gutterBottom>
-            <Header title={props.title} date={props.date} />
-          </Typography>
-          <Typography>
-            <Content content={props.content} />
-          </Typography>
-          <Typography>
-            <Image image={props.image} alt={props.alt} />
-          </Typography>
-        </CardContent>
+        <CardActionArea>
+          
+          <CardContent>
+            
+            <Typography className={'x2'} color="textSecondary" gutterBottom>
+              <Header title={props.title} date={props.date} />
+            </Typography>
+            
+            <Typography>
+              <Content content={props.content} />
+            </Typography>
+
+            <CardMedia
+              component="img"
+              image={`${props.image}`}
+              title={`${props.title}`}
+              alt={`${props.alt}`}
+              height="100%"              
+            />
+          </CardContent>
+
+        </CardActionArea>
       </Card>
 
     </div>
