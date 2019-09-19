6ab155fa9c21bee8283ae98b164785cd5235ce1f
rob-bee-neilson <robbie.neilson@gmail.com>
2019-09-19 23:35:26 +1200

    update Geekdeets.jsx

--- client/components/Geekdeets.jsx
+++ client/components/Geekdeets.jsx
@@ -4,31 +4,37 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
-const classes = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
+    backgroundColor: 'yellow',
   },
 }));
 
-// const classes = useStyles();
+export default function Geekdeets() {
+  const classes = useStyles();
 
-const Geekdeets = props => (
+  return (
 
-  <div className='boarder-geek'>
-    <Paper className={classes.root}>
-    {/* <Paper> */}
+    <div className='boarder-geek'>
+      
+      <Paper className={classes.root}>
+
         <Typography variant="h5" component="h5">
           This blog is:
         </Typography>
+
         <Typography component="p">
           v1.0:  React JS  (MVP - get it up and running!)
         </Typography>
+        <br></br>
+
         <Typography component="p">
           Dependencies:
         </Typography>
-        <Typography component="p">
+
+        <Typography component="ul">
           <ul>
-            <li>express: 4.16.2</li>
             <li>babel-core: 6.26.0</li>
             <li>babel-loader: 7.1.2</li>
             <li>babel-plugin-transform-class-properties: 6.24.1</li>
@@ -36,20 +42,26 @@ const Geekdeets = props => (
             <li>babel-preset-react: 6.24.1</li>
             <li>enzyme: 3.3.0</li>
             <li>enzyme-adapter-react-16: 1.1.1</li>
+            <li>express: 4.16.2</li>
             <li>jest: 22.1.4</li>
+            <li>material-ui/core: 4.4.0</li>
+            <li>material-ui/icons: 4.2.1</li>
             <li>react: 16.2.0</li>
             <li>react-dom: 16.2.0</li>
             <li>webpack: 3.10.0</li>
             <li>webpack-dev-server: 2.11.1</li>
-           </ul>
+          </ul>
+          
         </Typography>
+        <br></br>
+
         <Typography component="p">
           Deployed on Heroku.
         </Typography>
+
       </Paper>
 
-  </div>
+    </div>
   
-)
-
-export default Geekdeets
+);
+}
